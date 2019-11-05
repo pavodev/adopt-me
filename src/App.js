e650135bdb2f705b381e74a4cde502e6011cc2d1
pavodev <pavicivan.ip@gmail.com>
2019-11-05 18:20:41 +0100

    added parcel support and split App and Pet components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-/*
-    Props
-*/
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//         React.createElement("h1", {}, props.name),
-//         React.createElement("h2", {}, props.animal),
-//         React.createElement("h2", {}, props.breed)
-//     ]);
-// }
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
-/*
-    Destructuring props
-*/
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
-
-// createElement is
 /*
     App is like a rubber stamp. It allows as to stamp its content as many
     times we want. Every time we use app, a div is stamper (created) and inside 
@@ -49,4 +30,4 @@ const App = () => {
 };
 
 // Rendering to the DOM, we also say where we want App to be rendered
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+/*
+    Props
+*/
+// const Pet = (props) => {
+//     return React.createElement("div", {}, [
+//         React.createElement("h1", {}, props.name),
+//         React.createElement("h2", {}, props.animal),
+//         React.createElement("h2", {}, props.breed)
+//     ]);
+// }
+
+/*
+    Destructuring props
+*/
+export default function Pet({ name, animal, breed }) {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+}
