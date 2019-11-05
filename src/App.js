487572f7e58d5c41a8ef513066c59d8f46745941
pavodev <pavicivan.ip@gmail.com>
2019-11-05 16:56:17 +0100

    added npm support and installed some dev dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 /*
     Props
 */
@@ -14,14 +13,14 @@
     Destructuring props
 */
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
 
-// createElement is 
+// createElement is
 /*
     App is like a rubber stamp. It allows as to stamp its content as many
     times we want. Every time we use app, a div is stamper (created) and inside 
@@ -29,26 +28,25 @@ const Pet = ({ name, animal, breed }) => {
     Inside the curly brackets we put the attributes we want the div to have.
 */
 const App = () => {
-    return React.createElement(
-        "div",
-        { id: "something-important" },
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { 
-                name: "Luna", 
-                animal: "Dog", 
-                breed: "Havanese" }),
-            React.createElement(Pet, { 
-                name: "Pepper", 
-                animal: "Bird", 
-                breed: "Cockatiel" }),
-            React.createElement(Pet, { 
-                name: "Doink", 
-                animal: "Cat", 
-                breed: "Mixed" })
-        ]
-    );
-}
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed"
+    })
+  ]);
+};
 
 // Rendering to the DOM, we also say where we want App to be rendered
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
