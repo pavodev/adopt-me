b1ec0a4683f271decb4f6d45b13d1be4322c83d6
pavodev <pavicivan.ip@gmail.com>
2019-11-06 19:34:13 +0100

    added Reach Router support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something important">Adopt me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">
+            <h1 id="something important">Adopt me!</h1>
+          </Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
---
+++ src/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = props => {
+  return <h1>HI LOL</h1>;
+};
+
+export default Details;
