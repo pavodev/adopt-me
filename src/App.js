a7d0cf2262b7dc10c28f5c0c7230e493d1477edb
pavodev <pavicivan.ip@gmail.com>
2019-11-07 15:49:28 +0100

    implemented some code splitting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
 import { Router } from "@reach/router";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
+
+// LAZY
+// with lazy, Details is imported only when it is going to be rendered
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
+
+// SUSPENSE
+// Displays something till the content is rendered
 
 const App = () => {
   // if we want to pass multiple entries with context, the best thing to do
@@ -16,10 +22,12 @@ const App = () => {
       <ThemeContext.Provider value={themeHook}>
         <div>
           <NavBar />
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <Suspense fallback={<h1>Loading route...</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
--- src/NavBar.js
+++ src/NavBar.js
@@ -12,7 +12,6 @@ const Spin = keyframes`
 const NavBar = () => (
   <header
     css={css`
-      background-color: ${colors.dark};
       padding: 15px;
       margin-bottom: 15px;
       border-radius: 0 0 5px 5px;
